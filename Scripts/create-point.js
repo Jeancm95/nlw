f6f8cea844de5b709413e954c9c6c15440d150cf
Your Name <you@example.com>
2020-06-06 01:04:50 -0300

    NLW começando aula 3

--- Scripts/create-point.js
+++ Scripts/create-point.js
@@ -1,8 +1,6 @@
-
-
 function populateUFs() {
     const ufSelect = document.querySelector("select[name=uf]")
-       
+   
     fetch("https://servicodados.ibge.gov.br/api/v1/localidades/estados")
     .then( (res) => { return res.json() })
     .then( states => {
@@ -25,19 +23,26 @@ function getCities(event) {
     const indexOfSelectedState = event.target.selectedIndex
     stateInput.value = event.target.options[indexOfSelectedState].text
 
+    
     const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`
+
+
+    citySelect.innerHTML = "<option value>Selecione a Cidade</option>"
+    citySelect.disabled = true
 
     fetch(url)
     .then( (res) => { return res.json() })
     .then( cities => {
+       
+        
         for( const city of cities ) {
-            citySelect.innerHTML += `<option value="${city.id}">${city.nome}</option>`
+            citySelect.innerHTML += `<option value="${city.nome}">${city.nome}</option>`
         }
 
         citySelect.disabled = false
     } )
 
-
+    
 }
 
 document
